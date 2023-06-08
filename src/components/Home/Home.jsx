20387c645643bfde72e82d735c1f030d9f586954
Vladimir4123 <vladimirbn24@gmail.com>
2023-06-08 15:52:35 +0900

    fix: eslintrc feat: add Home.jsx feat: add Home.css feat: add Home.text.js feat: add logo.svg feat: add NotFound.jsx feat: add not-found.css feat: add Root.jsx feat: add index.css feat: add index.js feat: add reducers.js feat: add constants.js

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,2 @@
+import logo from "./assets/images/logo.svg";
+import "./assets/styles/home.css";
--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import appReducer from "./reducers";
+import Root from "./components/Root/Root";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
+ReactDOM.render(
+  <Provider store={store}>
+    <Root />
+  </Provider>,
+  document.getElementById("root")
+);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+// creating store with our application reducer as state
+let store = createStore(appReducer);
+
+ReactDOM.render(
+  // wrapping our App component inside Provider
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
+
+//registerServiceWorker();
