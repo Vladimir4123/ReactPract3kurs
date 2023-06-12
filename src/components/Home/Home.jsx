7acba8225b8a0ba560cb55876542070d3566f751
Vladimir4123 <vladimirbn24@gmail.com>
2023-06-12 15:55:55 +0900

    fix: function in Home.jsx feat: footer in Home.jsx feat: header in Home.jsx feat: products in Home.jsx feat: add images links in Home.jsx feat: add products links in Home.jsx refactor: delete logo.svg refactor: delete constants.js refactor: delete Home.css refactor: delete Home.test.js refactor: delete NotFound.jsx refactor: delete not-found.css refactor: delete Root.jsx refactor: delete index.css refactor: delete unnecessary folders

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,2 +1,90 @@
-import logo from "./assets/images/logo.svg";
-import "./assets/styles/home.css";
+import React, { useEffect, useState } from "react";
+
+function Header() {
+  const headerStyle = {
+    backgroundColor: "black",
+    padding: "20px",
+    textAlign: "center",
+    color: "white",
+  };
+
+  return (
+    <header style={headerStyle}>
+      <h1>BVN</h1>
+    </header>
+  );
+}
+
+function Footer() {
+  const footerStyle = {
+    backgroundColor: "black",
+    padding: "10px",
+    textAlign: "center",
+    color: "white",
+  };
+
+  return (
+    <footer style={footerStyle}>
+      <p>Все права защищены &copy; 2023</p>
+    </footer>
+  );
+}
+
+function Home() {
+  const [products, setProducts] = useState([]);
+
+  const getProducts = async () => {
+    await fetch("https://dummyjson.com/products")
+      .then((resp) => resp.json())
+      .then((resp) => {
+        console.log(resp);
+        setProducts(resp.products);
+      });
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  const productContainerStyle = {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "center",
+  };
+
+  const productStyle = {
+    width: "200px",
+    margin: "10px",
+  };
+
+  const productTitleStyle = {
+    fontWeight: "bold",
+  };
+
+  const productImageStyle = {
+    width: "100%",
+    height: "auto",
+  };
+
+  return (
+    <div>
+      <Header />
+      <div style={productContainerStyle}>
+        {products.map((item) => (
+          <div key={item.id} style={productStyle}>
+            <p style={productTitleStyle}>Товар: {item.title}</p>
+            <p>Цена: {item.price}</p>
+            <img
+              src={item.thumbnail}
+              alt={item.title}
+              style={productImageStyle}
+            />
+          </div>
+        ))}
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,6 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { Provider } from "react-redux";
-import { createStore } from "redux";
-import appReducer from "./reducers";
-import Root from "./components/Root/Root";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -14,25 +10,9 @@ root.render(
     <App />
   </React.StrictMode>
 );
-ReactDOM.render(
-  <Provider store={store}>
-    <Root />
-  </Provider>,
-  document.getElementById("root")
-);
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 // creating store with our application reducer as state
-let store = createStore(appReducer);
-
-ReactDOM.render(
-  // wrapping our App component inside Provider
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
-);
-
-//registerServiceWorker();
